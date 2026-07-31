564fc91c7da74a459828715f7b3870bfb847d3c9
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-07-31 09:26:14 +0100

    Fix: display Id naming and increment bug

---
+++ script.js
@@ -0,0 +1,16 @@
+// BUG 1: The ID doesn't match the HTML element! Fix "counter-display-num"
+const display = document.getElementById("count");
+const incrementBtn = document.getElementById("increment-btn");
+const decrementBtn = document.getElementById("decrement-btn");
+const resetBtn = document.getElementById("reset-btn");
+
+
+incrementBtn.addEventListener("click", () => {
+  count++;
+  // BUG 2: Updates .value instead of text content... why?
+  display.innerText = count;
+});
+
+
+// JS TASK 1: Add event listener for decrementBtn to decrease count (don't go below 0!)
+// JS TASK 2: Add event listener for resetBtn to set count back to 0
